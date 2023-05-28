1c3e7176054b17d6c39eca7bc131b219622c614e
Cem Fidan <devcfidan@gmail.com>
2023-05-28 19:48:10 +0300

    core operations and some features added

--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -1,18 +1,15 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal : EfEntityRepositoryBase<Car, LocalDatabaseContext>, ICarDal
+    public class EfColorDal : EfEntityRepositoryBase<Color, LocalDatabaseContext>, IColorDal
     {
-
     }
 }
