c721e035e2d342021cf57590485222752c5fad39
Cem Fidan <devcfidan@gmail.com>
2023-05-30 10:21:08 +0300

    webapi layer operations added

--- DataAccess/Concrete/EntityFramework/LocalDatabaseContext.cs
+++ DataAccess/Concrete/EntityFramework/LocalDatabaseContext.cs
@@ -15,9 +15,12 @@ namespace DataAccess.Concrete.EntityFramework
         {
             optionsBuilder.UseSqlServer(@"Server=DESKTOP-5SH8PML;Database=RentACar;trusted_connection=true;encrypt=false");
         }
-        public DbSet<Car> Cars { get; set; }
         public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
         public DbSet<Color> Colors { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<CarDetailDto> GetCarDetails { get; set; }
     }
 }
